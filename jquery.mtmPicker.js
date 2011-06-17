7293d325cea151551e849af061f65a2aafbf11e9
Ryan Bales <rbales@andy-838.local>
2011-06-17 10:32:30 -0500

    initial load

---
+++ jquery.mtmPicker.js
@@ -0,0 +1,75 @@
+/**
+* jQuery Plugin to easily manage ajax creation of many-to-many widgets in a form
+* @author Ryan Bales
+* Usage:
+			$(".initial-add-anchor").mtmPicker({
+				source : '/controller/ajaxCreationAction',
+				container : $('#parent-container'),
+				remove_label : 'Remove this element',
+				delete_widget : true
+			});
+**/
+(function($) {
+
+	$.fn.mtmPicker = function(options) {
+
+		var defaults = {
+			delete_widget : false,
+			remove_label : 'Remove',
+			source : null, // must be set at runtime
+			container : null // must be set at runtime
+		};
+
+		var options = $.extend(defaults,options);
+		// initialize widget pointer
+		var widget_block_pointer = 0;
+		if( options.delete_widget === true ) { 
+			$('div[class^=mtm]').each(function() {
+				var pointer = parseInt($(this).attr('class').replace(/^mtm(.*)/,'$1'));
+				if( !isNaN(pointer) ) {
+					if( pointer >= widget_block_pointer ) {
+						widget_block_pointer = pointer + 1;
+					}
+				}
+			});
+		}
+
+		var remove_mtm_block = function() {
+			$(this).closest('.mtm' + widget_block_pointer).remove();
+		}
+
+		var addNewElementSet = function($node) {
+			$.get(options.source,function(response,status) {
+				if( status === 'success' ) {
+					if( options.delete_widget === false ) {
+						$(response).insertBefore($node);
+					}
+					else if ( options.delete_widget === true ) {
+						$('<div/>')
+							.attr('class','mtm' + widget_block_pointer)
+							.append(response)
+							.append($('<a/>')
+									.attr('class','delete-mtm')
+									.text(options.remove_label)
+									.css('cursor','pointer')
+									.css('margin-left','5px')
+							)
+							.insertBefore($node);
+						// bind anchor to remove function
+						options.container.find('.mtm' + widget_block_pointer + ':last').find('a.delete-mtm').click(remove_mtm_block);
+					}
+				}
+			});
+		};
+
+
+		this.each(function() {
+			(function($node) {
+				$node.click(function() {
+					addNewElementSet($( this ));
+				});
+			})($( this ));	
+		});
+	};
+
+})(jQuery);
